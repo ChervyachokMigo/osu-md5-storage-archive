2b2ed9e6e9669d2616b7d49280bc45def76f0d3c
ChervyachokMigo <github@svdgod.ru>
2024-10-23 18:37:32 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+const beatmaps = require("./beatmaps");
+const misc = require("./misc");
+const requests = require("./requests");
 const storage = require("./storage");
 
-module.exports = storage;
+module.exports = {
+	...misc,
+	...requests,
+	...beatmaps,
+	...storage
+};
